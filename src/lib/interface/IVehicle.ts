1314e9e6201d87b5f53bad2270a8d6dd933f6036
Janos Vajda <vajdajanos@gmail.com>
2021-02-17 10:28:53 +0000

    Missing comments has been added to IVehicle

--- src/lib/interface/IVehicle.ts
+++ src/lib/interface/IVehicle.ts
@@ -1,19 +1,49 @@
 import {IParcel} from '../interface/IParcel';
 
+/**
+ * Vehicle interface.
+ */
 export interface IVehicle {
 
+    /**
+     * Returns by vehicle ID
+     * @var string
+     */
     getId(): string;
 
+    /**
+     * Set vehicle ID
+     * @param value
+     */
     setId(value: string);
 
+    /**
+     * Vehicle weight without parcels.
+     * @var number
+     */
     getWeight(): number;
 
+    /**
+     * Set weight of vehicle.
+     * @param value
+     */
     setWeight(value: number);
 
+    /**
+     * Load new parcel to the vehicle.
+     * @param value
+     */
     addParcel(value: IParcel);
 
+    /**
+     * Remove parcel from vehicle.
+     * @param id
+     */
     removeParcel(id: number);
 
+    /**
+     * List all parcels.
+     */
     listParcels(): object;
 
 }
