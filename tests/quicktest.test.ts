9319693c36a75e85974dd67bad0469b9f51e8b7a
Janos Vajda <vajdajanos@gmail.com>
2021-02-17 08:32:03 +0000

    Express has been installed. New directory structure was created

--- tests/quicktest.test.ts
+++ tests/quicktest.test.ts
@@ -2,11 +2,11 @@ const chai = require('chai');
 
 const assert = chai.assert;
 
-import {Calculator} from '../lib/Calculator';
+import {Calculator} from '../src/lib/Calculator';
 
 describe('calculate', function () {
     it('add', function () {
         let c = new Calculator();
-        assert.equal(-1, [1, 2, 3].indexOf(4));
+        assert.strictEqual(c.sum(5, 2), 7);
     });
 });
