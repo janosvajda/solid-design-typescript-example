558f798fe8b6a5b1cf110c56d7ccd3f2e62676d6
Janos Vajda <vajdajanos@gmail.com>
2021-02-18 04:24:13 +0000

    Code cleaning: missing comments has been added, reformatting code

--- src/routes/trucks.ts
+++ src/routes/trucks.ts
@@ -1,23 +1,21 @@
 import {TruckBig} from "../lib/class/TruckBig";
 
 module.exports = function (app, router, trucks) {
-
     /**
-     * List all trucks
+     * List all trucks.
      */
     app.get('/trucks', function (req, res) {
         res.end(JSON.stringify(trucks));
     });
 
     /**
-     * Create new truck.
+     * Create a new truck.
      */
     router.post('/trucks', function (req, res) {
-        console.log(req.body);
+
         if (req.body.id !== undefined && req.body.weight !== undefined) {
 
             let searchTruckByID = trucks.find(x => x.id === req.body.id);
-            console.log('searchTruckByID', searchTruckByID)
 
             if (searchTruckByID === undefined) {
                 let truck = new TruckBig();
@@ -28,7 +26,7 @@ module.exports = function (app, router, trucks) {
             } else {
                 res.write(JSON.stringify('Error: this truck ID is exists.'));
             }
-            console.log('All trucks:', trucks);
+
         } else {
             res.write(JSON.stringify('truck ID & truck weight are required.'));
         }
