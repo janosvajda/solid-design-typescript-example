872561e23e346899d7ea8cef71603c933c66e23d
Janos Vajda <vajdajanos@gmail.com>
2021-02-18 10:01:02 +0000

    Code cleanng: base class of vehicle has been created

---
+++ src/lib/class/BaseVehicle.ts
@@ -0,0 +1,141 @@
+import {IParcel} from '../interface/IParcel';
+import {IVehicle} from '../interface/IVehicle';
+
+/**
+ * @class BaseVehicle
+ */
+export class BaseVehicle implements IVehicle {
+
+    /**
+     * @private
+     */
+    private parcels: Array<IParcel>;
+
+    /**
+     * @private
+     */
+    private id: string;
+
+    /**
+     * @private
+     */
+    private weight: number;
+
+    constructor() {
+        this.parcels = new Array<IParcel>();
+    }
+
+    addParcel(parcel: IParcel) {
+
+        let parcels = this.getParcels();
+        let parcelItem = (Object.values(parcels)).find(p => p.getParcelId() === parcel.getParcelId());
+
+        if (parcelItem === undefined) {
+            this.parcels.push(parcel);
+        } else {
+            throw 'Parcel has been added to this vehicle';
+        }
+    }
+
+    /**
+     * Return by truck id.
+     */
+    getId(): string {
+        return this.id;
+    }
+
+    /**
+     * Returns by all added parcels.
+     */
+    getParcels(): object {
+        return this.parcels;
+    }
+
+    setParcels(parcels) {
+        this.parcels = parcels;
+    }
+
+    /**
+     * Returns by the vehicle weight without parcels.
+     */
+    getWeight(): number {
+        return this.weight;
+    }
+
+    /**
+     * Remove parcel from the vehicle.
+     * @param id
+     */
+    removeParcel(id: number) {
+
+        const removeItem = (removeId) => {
+            const res = Object.values(this.getParcels()).filter(obj => obj.id !== removeId);
+            return res;
+        }
+        this.setParcels(removeItem(id));
+    }
+
+    setId(value: string) {
+        this.id = value;
+    }
+
+    setWeight(value: number) {
+        this.weight = value;
+    }
+
+    /**
+     * Weight of all loaded parcels.
+     */
+    getLoadedParcelWeight(): number {
+        let result = 0;
+        Object.values(this.getParcels()).forEach(parcel => {
+            result += Number(parcel.getWeight());
+        })
+        return result;
+    }
+
+    /**
+     * Count of all loaded parcels.
+     */
+    getLoadedParcelCount(): number {
+        return Object.values(this.getParcels()).length
+    }
+
+    /**
+     * Each parcels must have unique parcelId on the truck.
+     * @deprecated This is deprectaed and not in use anymore.
+     */
+    hasParcelsUniqueId() {
+        let uniqueItems = []
+
+        Object.values(this.getParcels()).forEach(parcel => {
+            uniqueItems.push(parcel.getParcelId());
+        });
+
+        if (new Set(uniqueItems).size !== Object.values(this.getParcels()).length) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Parcels checking on the vehicle.
+     */
+    checkParcels(): boolean {
+
+        Object.values(this.getParcels()).forEach(parcel => {
+            try {
+                parcel.getValidator().validate();
+            } catch (e) {
+            }
+        });
+
+        //Truck weight must be added
+        if (this.getWeight() === undefined || this.getWeight() === null) return false;
+
+        return true;
+    }
+
+
+}
